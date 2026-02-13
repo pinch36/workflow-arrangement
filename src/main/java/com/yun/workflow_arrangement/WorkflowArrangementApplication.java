3b8450967c5082aec18569e31562bed63cab270a
raoliwen <raoliwen@xiaohongshu.com>
2026-02-13 15:31:56 +0800

    feat:init

---
+++ src/main/java/com/yun/workflow_arrangement/WorkflowArrangementApplication.java
@@ -0,0 +1,13 @@
+package com.yun.workflow_arrangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowArrangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowArrangementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yun/workflow_arrangement/WorkflowArrangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.workflow_arrangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowArrangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
